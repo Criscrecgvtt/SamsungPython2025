d15f7147e81c6a9b4d7628a15b5ac125f1e7f19f
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-16 13:49:26 +0200

    Dia1

---
+++ Semana_1/Dia_1/Presentacion.py
@@ -0,0 +1,7 @@
+print("*"*36)
+print("*","  Hola! deja que me presente    ","*")
+print("*","  Nombre: Cristina              ","*")
+print("*","  Trabajo:NO                    ","*")
+print("*","  Hobbys: MIAU                  ","*")
+
+print("*"*36)
