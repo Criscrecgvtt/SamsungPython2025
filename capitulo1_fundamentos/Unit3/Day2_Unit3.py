9e06fc2540c564ecb873b7452dd5e4ac81bcfb64
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-17 12:21:27 +0200

    Unit3_exercises

---
+++ capitulo1_fundamentos/Unit3/Day2_Unit3.py
@@ -0,0 +1,9 @@
+#Secuencias de escape en Strings
+
+print("Hola \"Javi \"") #Lo pongo entre barras para poder incluir esas comillas, ya que en principio son caracteres reservados
+print("Pon un tabulador \t\t\t aquí") #\t = tabular
+
+#Indexado en python
+"hello"[0]
+"hello"[-1] #accedes al ultimo caracter
+
