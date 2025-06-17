854b535a6760a83ab13c7c29be33ad3a28939f30
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-17 11:48:56 +0200

    Update day2.py

---
+++ capitulo1_fundamentos/Unit_2/Day2.py
@@ -0,0 +1,9 @@
+#Comando type para saber el tipo de una variable
+print(type(False))
+
+#Precedencia de operadores
+10+20*30 #Como en matemáticas basicamente
+
+#Data type conversions
+int('10')/2 #Convert string '10' into integer and divide by 2
+str(3.14) #Convert to String
