fe2aa6eec8e72817ac5047c80b5b65cf9bb7a3d0
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-17 11:58:21 +0200

    Questions 1 and 2- Unit 3

---
+++ Unit3/Q1_paperCoding.py
@@ -0,0 +1,5 @@
+#With 50 (int) and 50 int get 100
+#With 50 (int) and 50 int get "5050"
+
+print(int('50')+50)
+print("50"+str(50))
---
+++ Unit3/Q2_paperCoding.py
@@ -0,0 +1,5 @@
+#One letter 1 and three 0s given,in string format,
+# use these twoo two make the number 1000,
+# only use the function int once
+
+print(int('1'+'0'+'0'+'0'))
