3cb7017f5adb03681f34314835420ffd453e7504
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-18 10:05:15 +0200

    Update exercises Unit 4

--- capitulo1_fundamentos/Unit_4/Q2_pairProgramming.py
+++ capitulo1_fundamentos/Unit_4/Q2_pairProgramming.py
@@ -1,13 +1,13 @@
 #Hacer la tabla de los cuadrados de 2 a 6
 a = 2
 n = 2
-print("a"," "*4,"n"," "*4,"a**n")
-print(a," "*4,n," "*4,a**n)
+print("a","n","a**n",sep="\t\t")
+print(a,n,a**n,sep="\t\t")
 a+=1
-print(a," "*4,n," "*4,a**n)
+print(a,n,a**n,sep="\t\t")
 a+=1
-print(a," "*4,n," "*4,a**n)
+print(a,n,a**n,sep="\t\t")
 a+=1
-print(a," "*4,n," "*4,a**n)
+print(a,n,a**n,sep="\t\t")
 a+=1
-print(a," "*4,n," "*4,a**n)
+print(a,n,a**n,sep="\t\t")
--- capitulo1_fundamentos/Unit_4/Q2_pairProgramming_Advanced.py
+++ capitulo1_fundamentos/Unit_4/Q2_pairProgramming_Advanced.py
@@ -2,8 +2,8 @@
 a = 2
 n = 2
 count = 5
-print("a"," "*4,"n"," "*4,"a**n")
+print("a","n","a**n",sep="\t\t")
 while count >0:
-    print(a," "*4,n," "*4,a**n)
+    print(a,n,a**n,sep="\t\t")
     a+=1
     count-=1
