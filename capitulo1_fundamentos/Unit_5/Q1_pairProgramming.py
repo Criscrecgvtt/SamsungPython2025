d4a3364b1ca22d1924e2380057fffaad4e695df4
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-18 11:06:26 +0200

    Update Q1_pairProgramming.py

--- capitulo1_fundamentos/Unit_5/Q1_pairProgramming.py
+++ capitulo1_fundamentos/Unit_5/Q1_pairProgramming.py
@@ -1,3 +1,7 @@
 num = int (input("Enter a 3-digit integer: "))
 div = num//100 #El operador // es la division entera
-print(div==3)
+print("Hay un tres en las centenas?",div==3)
+
+#Another way , it needs more checkings
+num =  input("Enter a 3-digit integer: ")
+print("Hay un tres en las centenas?",num[0]=='3')
