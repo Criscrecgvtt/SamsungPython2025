7cede343bd2d09fc16e010ed34e2d4ba8722aa5f
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-18 10:41:49 +0200

    Unit 5 PaperCoding Exercises

---
+++ capitulo1_fundamentos/Unit_5/Q1_paperCoding.py
@@ -0,0 +1,2 @@
+num = int(input("Dame un numero: "))
+print("Es el numero par?\n",num%2==0)
---
+++ capitulo1_fundamentos/Unit_5/Q2_paperCoding.py
@@ -0,0 +1,2 @@
+num = int(input("Dame un numero: "))
+print("Esta el número entre 0 y 100?\n",0 <= num <= 100)
