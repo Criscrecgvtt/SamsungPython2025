0daf36a6e6981ecd0ad3727b23f8148bcd500121
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-18 13:04:58 +0200

    Unit 6 paperCoding exercises

---
+++ capitulo1_fundamentos/Unit_6/Q1_paperCoding.py
@@ -0,0 +1,5 @@
+#If use
+gameScore = int(input("Enter game score: "))
+print("game_score =",gameScore)
+if gameScore > 1000:
+    print("You are a master")
---
+++ capitulo1_fundamentos/Unit_6/Q2_paperCoding.py
@@ -0,0 +1,5 @@
+#If use
+integer = int(input("Enter an integer: "))
+print("x =",integer)
+if integer > 0:
+    print(integer,"is a natural number.")
