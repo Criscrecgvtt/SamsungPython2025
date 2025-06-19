68f0a6d828f2fee86eaad16d46cd8992d381241a
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-19 11:24:44 +0200

    paperCoding Unit8

---
+++ capitulo1_fundamentos/Unit_8/Q3_paperCoding.py
@@ -0,0 +1,4 @@
+even =0
+for i in range(0,101,2):
+    even+=i
+print("Sum of even numbers from 1 to 100:",even)
---
+++ capitulo1_fundamentos/Unit_8/Q4_paperCoding.py
@@ -0,0 +1,4 @@
+odd =0
+for i in range(1,101,2):
+    odd+=i
+print("Sum of odd numbers from 1 to 100:",odd)
