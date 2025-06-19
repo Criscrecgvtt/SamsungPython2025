63a06ed85b7161f6519a0f52abd3708c783f6098
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-19 09:56:47 +0200

    Create Q1_pairProgramming.py

---
+++ capitulo1_fundamentos/Unit_7/Q1_pairProgramming.py
@@ -0,0 +1,18 @@
+print("1)Addition","\t","2) Substraction","\t","3) Multiplication","\t","4) Division")
+op_num=int(input("Enter the desire number of operation : "))
+op_types =[1,2,3,4]
+if not(op_num in op_types):
+    print("The operation selected is not valid")
+else:
+    a,b= input("Enter two numbers for the operation: ").split()
+    a,b = int(a),int(b)
+    print(a)
+    print(b)
+    if op_num == 1:
+        print(a,"+",b,"=",a+b)
+    elif op_num == 2:
+        print(a,"-",b,"=",a-b)
+    elif op_num == 3:
+        print(a,"*",b,"=",a*b)
+    else:
+        print(a,"/",b,"=",a/b)
