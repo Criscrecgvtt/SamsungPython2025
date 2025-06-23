b004eb82f6147eff2b2bc04546ca973cef468287
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-23 11:28:16 +0200

    Update Q1_pairProgramming.py

--- capitulo1_fundamentos/Unit_9/Q1_pairProgramming.py
+++ capitulo1_fundamentos/Unit_9/Q1_pairProgramming.py
@@ -1,10 +1,8 @@
 number = input("Enter an integer: ")
-i = 1
+length = len(number)
 
-for n in number:
-    if n == number[-i]:
-        i += 1
-    else:
+for i in range(length // 2):
+    if number[i] != number[-(i + 1)]:
         print(number, "is not a palindrome number")
         break
 else:
