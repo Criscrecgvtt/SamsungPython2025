38b309b004cd3e5ff58041f48a5a32d01ab015ac
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-25 13:52:47 +0200

    paperCoding Unit10

---
+++ capitulo2_estructuras/Unit_10/Q1_paperCoding copy.py
@@ -0,0 +1,3 @@
+primes = [2,3,5,7]
+
+print("El primer elemento de la lista es",primes[0])
---
+++ capitulo2_estructuras/Unit_10/Q2_paperCoding copy.py
@@ -0,0 +1,7 @@
+primes = [2,3,5,7]
+
+print("Primes numbers: ",primes)
+
+primes.append(11)
+
+print("Primes numbers after edition: ",primes)
