ba72c6932a9316c900bab174afc85491f4a1982c
Criscrecgvtt <crismendez.mir@gmail.com>
2025-06-30 12:38:31 +0200

    Create notes_unit12.py

---
+++ capitulo2_estructuras/Unit12/notes_unit12.py
@@ -0,0 +1,13 @@
+# El slicing no funciona con rangos, osea si funciona pero  devuelve rangos
+
+r = range(10,100,4)
+print(r)
+print(r[9])
+print(r[9:12])
+
+lista = list(r)
+lista2 = list(r[9:12])
+print(lista)
+print(lista2)
+
+#Todos los elementoa menos los n últimos 
