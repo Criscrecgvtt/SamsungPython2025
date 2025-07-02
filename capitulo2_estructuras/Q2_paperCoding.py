575e3cb7ebc0b7f854f724780b2992b8fb44203f
Criscrecgvtt <crismendez.mir@gmail.com>
2025-07-02 10:58:09 +0200

    paperCoding Unit 14

---
+++ capitulo2_estructuras/Q2_paperCoding.py
@@ -0,0 +1,5 @@
+items = {"Coffee": 7, "Pen":3, "Paper cup": 2, "Milk": 1, "Coke": 4, "Book":5}
+
+item_name = input("Enter the name of the item: ")
+
+print(items[item_name])
---
+++ capitulo2_estructuras/Unit_14/Q1_paperCoding.py
@@ -0,0 +1,6 @@
+person_dic = {'Last Nme':'Doe',
+             'First Name':'David',
+             'Company': 'Samsung'}
+
+for str1,info in person_dic.items():
+    print(str1,':',info)
