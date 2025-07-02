09aac1e7961a202fe5134a296a7e3143a6d18623
Criscrecgvtt <crismendez.mir@gmail.com>
2025-07-02 10:59:05 +0200

    Update Q2_paperCoding.py

--- capitulo2_estructuras/Q2_paperCoding.py
+++ capitulo2_estructuras/Q2_paperCoding.py
@@ -2,4 +2,5 @@ items = {"Coffee": 7, "Pen":3, "Paper cup": 2, "Milk": 1, "Coke": 4, "Book":5}
 
 item_name = input("Enter the name of the item: ")
 
-print(items[item_name])
+print(items.get(item_name))
+#itmes[item_name]
