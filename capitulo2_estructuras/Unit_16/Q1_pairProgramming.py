e3c96998550a60cb4a5002dd44e9dd4f7d639c1b
Criscrecgvtt <crismendez.mir@gmail.com>
2025-07-07 10:53:26 +0200

    ejercicios chap_2

--- capitulo2_estructuras/Unit_16/Q1_pairProgramming.py
+++ capitulo2_estructuras/Unit_16/Q1_pairProgramming.py
@@ -4,10 +4,14 @@ aux_list = copy.deepcopy(mylist)
 for tup in aux_list:
     tup = set(tup)
     
+print(aux_list)
 print(mylist)
 a,b = input("Enter two numbers: ").split(" ")
 a,b = int(a),int(b)
-two_num = (a,b)
+two_num = set((a,b))
+
+print(two_num in mylist)
+print(two_num in aux_list)
 
 
 
